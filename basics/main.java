64b6bb7bd42ce60e1abeb65c78d612eea866c43c
abkux <kumarjhaabhishek2002@gmail.com>
2023-11-12 00:29:47 +0530

    first commit :rocket:.

--- basics/main.java
+++ basics/main.java
@@ -4,7 +4,7 @@ class Abku {
     }
 }
 
-// ! Another Way ( file name should me same as class name. )
+// ! Another Way ( file name should be same as class name )
 /*public class Abku {
     public static void main(String[] args) {
         System.out.println("Hello World - Abku!");
