99efe68f2408f90c879f110478bb9acaf0f98950
abkux <kumarjhaabhishek2002@gmail.com>
2023-11-12 00:26:18 +0530

    first java commit! :rocket:

---
+++ basics/main.java
@@ -0,0 +1,12 @@
+class Abku {
+    public static void main(String[] args) {
+        System.out.println("Hello World -  Abku!");
+    }
+}
+
+// ! Another Way ( file name should me same as class name )
+/*public class Abku {
+    public static void main(String[] args) {
+        System.out.println("Hello World - Abku!");
+    }
+}*/
